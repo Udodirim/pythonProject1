1ed9591fc1265a18c8a0a2e09f24e8048dc6aa38
Kelechi Udodirim Nwosu <5nwosk81@solent.ac.uk>
2022-05-11 16:30:46 +0100

    Learnt how to assign a variable and print

---
+++ variables.py
@@ -0,0 +1,10 @@
+# Check in a patient named John Smith into a hospital
+# He is 20 years old and is a new patient
+
+name = "John Smith"
+age = 20
+height = 1.26
+is_new = True
+
+print(name,age,height,is_new)
+
